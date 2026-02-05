2474661d3f322f4504f0e3efae08e0b358ca3b02
AlbertoWebDev <albertovazquezmonge@gmail.com>
2026-02-05 16:01:47 +0100

    Header photo added

--- crear.php
+++ crear.php
@@ -11,7 +11,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ranchers&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Formulario</h1>
+    <div class="headerContainer">
+        <img src="resources/header.png" alt="Header">
+        <h1>Formulario</h1>
+    </div>
     <form onsubmit="return validateLanguages()" action="guardar.php" method="POST" enctype="multipart/form-data">
         <!-- Labels -->
         <label for="name">Nombre</label>
